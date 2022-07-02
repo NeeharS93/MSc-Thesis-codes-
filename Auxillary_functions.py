74ad74282b76055109d3b8bac9fce4653408f80a
Neehar <95689335+NeeharS93@users.noreply.github.com>
2022-07-02 19:07:48 +0100

    Update Auxillary_functions.py

--- Auxillary_functions.py
+++ Auxillary_functions.py
@@ -6,19 +6,19 @@ def dt_max(model, Parameter_values, C_rates):
     sims = []
     for C_rate in C_rates:
         if C_rate < 2.1:
-         t_eval = np.linspace(0, 4000 / C_rate, 500)
-         dt_max = None
+          t_eval = np.linspace(0, 4000 / C_rate, 500)
+          dt_max = None
         elif C_rate < 4:
-         t_eval = np.linspace(0, 3500 / C_rate, 1000)
-         dt_max = 0.1
+          t_eval = np.linspace(0, 3500 / C_rate, 1000)
+          dt_max = 0.1
         elif C_rate < 8:
-         t_eval = np.linspace(0, 3000 / C_rate, 500)
-         dt_max = 0.001
+          t_eval = np.linspace(0, 3000 / C_rate, 500)
+          dt_max = 0.001
         else:
-         t_eval = np.linspace(0, 60, 500)
-         dt_max = 0.001
-    solver = pybamm.CasadiSolver(mode="safe", dt_max=dt_max, max_step_decrease_count= 15)
-    sim = pybamm.Simulation(model, parameter_values=Parameter_values, var_pts=var_pts, solver=solver, C_rate=C_rate)
-    sim.solve(t_eval)
-    sims.append(sim)
-    return sims
+          t_eval = np.linspace(0, 60, 500)
+          dt_max = 0.001
+        solver = pybamm.CasadiSolver(mode="safe", dt_max=dt_max, max_step_decrease_count= 15)
+        sim = pybamm.Simulation(model, parameter_values=Parameter_values, var_pts=var_pts, solver=solver, C_rate=C_rate)
+        sim.solve(t_eval)
+        sims.append(sim)
+        return sims
